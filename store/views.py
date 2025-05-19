ce7d6f0ca21446cf503e9a06871ab06ef5415264
unknown <tareqhasan2007@gmail.com>
2025-05-20 00:22:38 +0600

    Checkout

--- store/views.py
+++ store/views.py
@@ -11,13 +11,20 @@ def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        
+        items = order.orderitem_set.all() 
     else:
         items = []
-    context = {'items': items}
+        order = {'get_cart_total': 0, 'get_cart_item': 0}
+    context = {'items': items,'order': order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_item': 0}
+    context = {'items': items,'order': order}
     return render(request, 'store/checkout.html', context)
